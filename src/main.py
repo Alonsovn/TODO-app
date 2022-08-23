01f4aa14a29ad5a3b98c475dafc6e9578702ddb7
Alonsovn <alonsonh94@gmail.com>
2022-08-23 15:42:38 -0600

    Database setup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Depends
+from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+import models
+from db import DBContext
 
 app = FastAPI()
 
@@ -8,6 +13,17 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+def get_db():
+    with DBContext() as db:
+        yield db
+
+
 @app.get("/")
-def root():
-    return {"Setup": "up!"}
+def root(request: Request):
+    return templates.TemplateResponse("index.html",
+                                      {"request": request, "title": "Home"})
+
+
+@app.get("/task")
+def get_task(db: Session = Depends(get_db)):
+    return jsonable_encoder(db.query(models.Task.first()))
