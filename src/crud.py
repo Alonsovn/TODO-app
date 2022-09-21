60206eb87754e3c73319930a2f951bd70a999b54
Alonso <70250204+Alonsovn@users.noreply.github.com>
2022-09-20 21:27:13 -0600

    Add get tasks by user_id  function

--- src/crud.py
+++ src/crud.py
@@ -32,3 +32,13 @@ def create_user(db: session, user : schemas.UserCreate):
     db.refresh(db_user)
     
     return db_user
+
+def get_task_by_user_id(db: Session, id: str,  skip: int = 0, limit: int = 100):
+    return db.query(models.Task).filter(models.Task.user_id = id).offset(skip).limit(limit).all()
+
+
+
+
+
+
+
